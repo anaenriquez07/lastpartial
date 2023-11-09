a3211f2313d040bbc344c858ec3a309e41b63266
anaenriquez07 <145053427+anaenriquez07@users.noreply.github.com>
2023-11-09 17:46:52 -0600

    Create alexa_assistant.py

---
+++ alexa_assistant.py
@@ -0,0 +1,12 @@
+https://replit.com/join/wwpuavmfsw-ana-lucialuc103 
+
+Alexa = input()
+
+words = Alexa.split()
+
+number_alexas = words.count('Alexa')
+
+if number_alexas == "1":
+  print ("What can I help you with? ")
+else:
+  print ("Hey, just say my name once") 
